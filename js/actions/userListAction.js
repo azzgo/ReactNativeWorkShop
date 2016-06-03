034dafc7fed77a2e218198634ff878e6f1267991
Yishan Wen <wittywys@gmaill.com>
2016-06-03 22:39:20 +0800

    abort fetch api

--- js/actions/userListAction.js
+++ js/actions/userListAction.js
@@ -19,24 +19,6 @@ function mockHelper(num) {
 }
 
 export function fetchUserListAction() {
-  // return dispatch => {
-  //   fetch('http://facehub.net/api/users', {
-  //     headers: {
-  //       Cookie: 'token=38dqyWGFnHf6U6C2bYpwet'
-  //     }
-  //   })
-  //   .then(res => {
-  //     if (res.ok) {
-  //       return res.json()
-  //     }
-  //     throw res.error()
-  //   })
-  //   .then(json => dispatch({
-  //     type: FetchUser,
-  //     payload: json.users
-  //   }))
-  //   .catch(error => console.log('request error: ', error))
-  // }
   return {
     type: FetchUser,
     payload: mockHelper(20)
