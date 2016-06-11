5204d2337a242685b940d60764e36c815fe71bfc
Yishan Wen <wittywys@gmaill.com>
2016-06-11 09:28:30 +0800

    add first  test

---
+++ test/components/UserCard-spec.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import NavBar from '../../js/components/NavBar'
+
+describe('Test UserCard', () => {
+  it('should render stuff', () => {
+    const wrapper = shallow(<NavBar />)
+
+  })
+})
