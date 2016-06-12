26bc8c8c373adfac0a4f3d006be6058ae1dba138
Yishan Wen <wittywys@gmaill.com>
2016-06-12 10:07:14 +0800

    add remote dev tool

--- js/store/index.js
+++ js/store/index.js
@@ -1,6 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import devTools from 'remote-redux-devtools'
+
+const longger = createLogger()
 
 import reducers from '../reducers'
 
-export default createStore(reducers, compose(applyMiddleware(thunk)))
+export default createStore(reducers, compose(applyMiddleware(thunk, longger), devTools({
+  name: 'Android app', realtime: true,
+  hostname: 'localhost', port: 8000
+})))
